6d37188df6fe4b3f4625c01f58fc95012e263317
Abdollah Hosseini <146338547+AbdollahHosseini@users.noreply.github.com>
2025-11-23 12:56:43 +0000

    Remove CV download link from Home.jsx
    Removed the CV download link from the Home page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ export default function Home() {
     <header className="hero">
       <h1>Abdollah</h1>
       <p>Second-Year Computer Science @ University of Bath</p>
-      <a className="btn" href="/Abdollah-CV.pdf" download>Download CV</a>
     </header>
   )
 }
